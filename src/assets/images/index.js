4b048a2119a9b30507f5bd01247ba394966a99dd
moez <moezabdelkefi17@gmail.com>
2024-10-26 01:14:01 +0100

    add

---
+++ src/assets/images/index.js
@@ -0,0 +1,67 @@
+import logo from "./logoex.jpg";
+import logoLight from "./logoex.jpg";
+import bannerImgOne from "./banner/prod2.png";
+import bannerImgTwo from "./banner/prod3.png";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import saleImgOne from "./sale/sale1.jpg";
+import saleImgTwo from "./sale/sale2.jpg";
+import saleImgThree from "./sale/sale3.jpg";
+// ============== Products Start here ====================
+// New Arrivals
+import newArrOne from "./products/newArrival/new.jpg";
+import newArrTwo from "./products/newArrival/new2.jpg";
+import newArrThree from "./products/newArrival/new3.jpg";
+import newArrFour from "./products/newArrival/new4.jpg";
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/best1.jpg";
+import bestSellerTwo from "./products/bestSeller/best2.jpg";
+import bestSellerThree from "./products/bestSeller/best3.jpg";
+import bestSellerFour from "./products/bestSeller/best4.jpg";
+
+// Special Offers
+import spfOne from "./products/specialOffer/sp1.jpg";
+import spfTwo from "./products/specialOffer/sp2.jpg";
+import spfThree from "./products/specialOffer/sp3.jpg";
+import spfFour from "./products/specialOffer/sp4.jpg";
+
+// Year Product
+import productOfTheYear from "./products/banner1.png";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+};
---
+++ src/components/home/BestSellers/BestSellers.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Heading from "../Products/Heading";
+import Product from "../Products/Product";
+import {
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+} from "../../../assets/images/index";
+
+const BestSellers = () => {
+  return (
+    <div className="w-full pb-20">
+      <Heading heading="Our Bestsellers" />
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
+        <Product
+          _id="1011"
+          img={bestSellerOne}
+          productName="Old Money"
+          price="35.00"
+          color="البرتقالي"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1012"
+          img={bestSellerTwo}
+          productName="أحذية"
+          price="180.00"
+          color="أسود"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1013"
+          img={bestSellerThree}
+          productName="قميص"
+          price="25.00"
+          color="مختلط"
+          badge={true}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+        <Product
+          _id="1014"
+          img={bestSellerFour}
+          productName="قميص"
+          color="أسود"
+          badge={false}
+          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
+        />
+      </div>
+    </div>
+  );
+};
+
+export default BestSellers;
